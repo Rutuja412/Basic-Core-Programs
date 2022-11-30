eeec3748863d838704346d5684c576203d5dc3c0
Rutuja412 <rutujakadam4545@gmail.com>
2022-11-30 21:56:21 +0530

    check No. is odd or even

---
+++ OddEvenNum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OddEvenNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter a number :");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Entered number is Even number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("Entered number is Odd number ");
+                Console.Read();
+            }
+        
+        }
+    }
+}

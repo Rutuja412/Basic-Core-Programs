307b3ecb3a093d8f8c9d296ac37fc7b7fcf9ea4a
Rutuja412 <rutujakadam4545@gmail.com>
2022-11-30 21:40:16 +0530

    Adding Program

---
+++ ComputeQuoRem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ComputeQuoRem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, div, rem, quo = 0;
+            Console.WriteLine("Enter a number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Divisor :");
+            div = Convert.ToInt32(Console.ReadLine());
+
+            if (num > div)
+            {
+                while (num >= div)
+                {
+                    num = num - div;
+                    quo++;
+                }
+                rem = num;
+                Console.WriteLine("\nThe quotient is : " + quo + "\nThe remainder is :" + rem);
+            }
+            else
+                Console.WriteLine("Enter valid input");
+            Console.ReadLine();
+        }
+    }
+}

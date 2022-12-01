c0e567f4f1666ef67ec8b00d6f67b2428024927f
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-01 08:04:55 +0530

    swapping of two num

---
+++ swapping of 2 num/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace swapping_of_2_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("Enter first number :");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Enter second number : ");
+            num2 = int.Parse(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.Write("\nAfter swapping :");
+            Console.Write("\nFirst number :" + num1);
+            Console.Write("\nSecond number :" + num2);
+            Console.Read();
+        }
+    }
+}

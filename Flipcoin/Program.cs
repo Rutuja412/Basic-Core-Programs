93b6a4a82460d45f2864a383a6a1e7f5b8aac66c
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-01 13:18:21 +0530

    Adding Flipcoin

---
+++ Flipcoin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+
+namespace Flipcoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.WriteLine("Enter number to flip coin");
+            n = int.Parse(Console.ReadLine());
+            int CountHead = 0;
+            int CountTail = 0;
+
+            for(i=1;i<=n;i++)
+            {
+                Random random= new Random();
+                int num=random.Next(3);
+
+                if (num==1)
+                {
+                    Console.WriteLine("Heads");
+                    CountHead++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    CountTail++;
+                }
+            }
+            Console.WriteLine("number of Heads : {0}", CountHead);
+            Console.WriteLine("number of Tails  :{0}", CountTail);
+
+            double Heads = CountHead / (double)n * 100;
+            double Tails = CountTail / (double)n * 100;
+
+            Console.WriteLine(" percentage of heads  :{0}", Heads);
+            Console.WriteLine(" percentage of tails   :{0}", Tails);
+
+
+        }
+    }
+}
---
+++ HarmonicNo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HarmonicNo_
+{
+    internal class Program
+    {
+        static double nthHarmonic(int N)
+        {
+
+            float harmonic = 1;
+
+            for (int i = 2; i < N; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+            return harmonic;
+
+        }
+        static void Main()
+        {
+            int N = 10;
+            Console.WriteLine(nthHarmonic(N));
+        }
+        
+    }
+}
